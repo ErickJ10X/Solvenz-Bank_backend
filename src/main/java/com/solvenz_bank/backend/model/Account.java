d2a2e234ec8655d66770a7b670c2f31486513c48
erick <erickjo10x@gmail.com>
2025-03-04 14:59:28 +0100

    create model Account

---
+++ src/main/java/com/solvenz_bank/backend/model/Account.java
@@ -0,0 +1,26 @@
+package com.solvenz_bank.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_number")
+    private int accountNumber;
+    @Column(name = "balance", nullable = false)
+    private double balance;
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+}
